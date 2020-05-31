3bd3725671b82521f0ce57268151387dfb9d144a
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2020-05-31 21:46:08 +0300

    Object creation

--- src/chapter2/item4/UtilityClass.java
+++ src/chapter2/item4/UtilityClass.java
@@ -2,7 +2,7 @@ package chapter2.item4;
 
 // Noninstantiable utility class (Page 19)
 public class UtilityClass {
-    
+
     // Suppress default constructor for noninstantiability
     private UtilityClass() {
         throw new AssertionError();
