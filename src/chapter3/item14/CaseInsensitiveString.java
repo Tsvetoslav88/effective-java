1fe3e165e62c0de21ed3443cca21bba363be429e
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2020-07-03 22:33:34 +0300

    Ïmplementation of Comparable and Comparator

---
+++ src/chapter3/item14/CaseInsensitiveString.java
@@ -0,0 +1,33 @@
+package chapter3.item14;
+
+import java.util.Objects;
+
+// Single-field Comparable with object reference field  (Page 69)
+public class CaseInsensitiveString implements Comparable<CaseInsensitiveString> {
+
+    private String s;
+
+    public CaseInsensitiveString(String s) {
+        this.s = Objects.requireNonNull(s);
+    }
+
+    // Fixed equals method (Page 40)
+    @Override public boolean equals(Object o) {
+        return o instanceof CaseInsensitiveString &&
+                ((CaseInsensitiveString) o).s.equalsIgnoreCase(s);
+    }
+
+    @Override public int hashCode() {
+        return s.hashCode();
+    }
+
+    @Override public String toString() {
+        return s;
+    }
+
+    // Using an existing comparator to make a class comparable
+    @Override
+    public int compareTo(CaseInsensitiveString cis) {
+        return String.CASE_INSENSITIVE_ORDER.compare(s, cis.s);
+    }
+}
